7d464097b5e9ed0305a4457f194ce367aab8f8ef
David Kumar <david.dn.kumar@gmail.com>
2021-01-04 15:14:47 -0600

    change root view controller

--- nanny-near-me/Controller/LoginController.swift
+++ nanny-near-me/Controller/LoginController.swift
@@ -1,18 +1,19 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  nanny-near-me
 //
-//  Created by David Kumar on 1/3/21.
+//  Created by David Kumar on 1/4/21.
 //  Copyright © 2021 David Kumar. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
 
 
