8cc674cb4ecad3e9820facdc7281978cca94f02d
David Kumar <david.dn.kumar@gmail.com>
2021-01-05 19:22:57 -0600

    add roundedShadow view

--- nanny-near-me/View/RoundedShadow.swift
+++ nanny-near-me/View/RoundedShadow.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class RoundedShadow: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override func awakeFromNib() {
+        setupView()
     }
-    */
+
+    func setupView(){
+        
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width:0, height: 5.0)
+        self.layer.cornerRadius = 5.0
+    }
 
 }
