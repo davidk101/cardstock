868721fe2123339b1e72debc3ae7ee98d13d4b42
David Kumar <david.dn.kumar@gmail.com>
2021-01-05 18:57:05 -0600

    add gradient for logo view

---
+++ nanny-near-me/View/GradientView.swift
@@ -0,0 +1,29 @@
+//
+//  GradientView.swift
+//  nanny-near-me
+//
+//  Created by David Kumar on 1/5/21.
+//  Copyright © 2021 David Kumar. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView  {
+
+    let gradient = CAGradientLayer()
+    
+    // after interface builder is setup, this function is overriden
+    override func awakeFromNib() {
+        setupGradientView()
+    }
+    
+    func setupGradientView(){
+        
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.init(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = CGPoint.zero // top-left point of simulator 
+        gradient.endPoint = CGPoint(x:0 ,y:1) // covers the entire view vertically
+        gradient.locations = [0.8, 1.0] // first color takes 0-80% of UIView and second color takes 80-100% of view
+        self.layer.addSublayer(gradient)
+    }
+}
