5718a4223bbad34ad3bc41136502a78ace48bc86
David Kumar <david.dn.kumar@gmail.com>
2021-01-09 16:17:56 -0600

    add spinner subview

--- nanny-near-me/View/RoundedShadowButton.swift
+++ nanny-near-me/View/RoundedShadowButton.swift
@@ -21,24 +21,67 @@ class RoundedShadowButton: UIButton {
         self.layer.shadowOpacity = 0.3
         self.layer.shadowOffset = CGSize.zero // shadow wont move
     }
+    
+    override func awakeFromNib() {
+        setupView()
+    }
 
     func animateButton(shouldLoad: Bool, withMessage message: String?){
         
+        let spinner = UIActivityIndicatorView()
+        spinner.style = .medium
+        spinner.color = UIColor.darkGray
+        spinner.alpha = 0.0
+        spinner.hidesWhenStopped = true
+        
+        spinner.tag = 21
+         
+        
         if shouldLoad{
+            
+            self.addSubview(spinner)
             
             self.setTitle("", for: .normal) // removing title
             UIView.animate(withDuration: 0.2, animations: { // animating button to become a circle
                 
                 self.layer.cornerRadius = self.frame.height / 2 // making square into circle
                 
-                self.frame = CGRect(x: self.frame.midX - (self.frame.height / 2), y: self.frame.origin.y, width: self.frame.height, height: self.frame.height) // X coordinates ensure circle is in the center of screen - width and height makes into square 
+                self.frame = CGRect(x: self.frame.midX - (self.frame.height / 2), y: self.frame.origin.y, width: self.frame.height, height: self.frame.height) // X coordinates ensure circle is in the center of screen - width and height makes into square
                 
             }, completion: {(finished) in
                 
                 if finished == true{
                     
+                    spinner.startAnimating()
+                    
+                    spinner.center = CGPoint(x:self.frame.width/2 + 1, y:self.frame.width/2 + 1)
+                    UIView.animate(withDuration: 0.2) {
+                        spinner.alpha = 1.0
+                    }
+                    
                 }
             })
+            
+            self.isUserInteractionEnabled = false // ensures button is unclickable
+            
+        }
+        else{
+            self.isUserInteractionEnabled = true
+            
+            for subview in self.subviews{ // iterating through all subviews
+                
+                if subview.tag == 21{ // ensuring only the spinner subview is removed
+                    subview.removeFromSuperview()
+                }
+            }
+            
+            UIView.animate(withDuration: 0.2) {
+                self.layer.cornerRadius = 5.0
+                self.frame = self.originalSize! // force unwrapping here since it is an optional - SELF.originalSize needed to refer to correct var outside closure
+                self.setTitle(message, for: .normal)
+                
+            }
+            
         }
     }
 }
--- nanny-near-me/ViewController.swift
+++ nanny-near-me/ViewController.swift
@@ -11,12 +11,18 @@ import MapKit
 
 class ViewController: UIViewController , MKMapViewDelegate{
     
+
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var actionBtn: RoundedShadowButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self // setting mapview's delegate to be ViewController
     }
     
+    @IBAction func actionBtnWasPressed(_ sender: Any) {
+        
+        actionBtn.animateButton(shouldLoad: true, withMessage: nil)
+    }
     
 }
