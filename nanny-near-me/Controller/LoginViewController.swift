f91b6992c402ec685ae4b400f0e92aa2b426e520
David Kumar <david.dn.kumar@gmail.com>
2021-02-03 09:46:33 -0600

    add loginVC Firebase connection

--- nanny-near-me/Controller/LoginViewController.swift
+++ nanny-near-me/Controller/LoginViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var authBtn: RoundedShadowButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bindToKeyboard() // View is moved up as soon as keyboard is displayed when UITextField is tapped
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap(sender:)))
         self.view.addGestureRecognizer(tap)
+        
+        emailField.delegate = self
+        passwordField.delegate = self
     }
     
     @objc func handleScreenTap(sender: UITapGestureRecognizer){
@@ -23,4 +32,63 @@ class LoginViewController: UIViewController {
         
     }
     
+    @IBAction func authBtnWasPressed(_ sender: Any) {
+        
+        if emailField.text != nil && passwordField.text != nil{
+            
+            authBtn.animateButton(shouldLoad: true, withMessage: nil)
+            self.view.endEditing(true) // hiding keyboard
+            
+            
+            if let email = emailField.text, let password = passwordField.text{ // closure
+                
+                Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                    
+                    // logging in a currently existing user
+                    // only executed if 'user' exists
+                    if error == nil{
+                        if let user = user{
+                            
+                            if self.segmentedControl.selectedSegmentIndex == 0{ // i.e. hirer is selected
+                                
+                                let userData = ["provider": user.providerID] as [String: Any]
+                                
+                                DataService.instance.createFirebaseDBUser(uid: user.uid, userData: userData, isHelper: false)
+                            }
+                            
+                            else{ // i.e. helper is selected
+                                
+                                let userData = ["provider":user.providerID, "userIsDriver": true, "isPickUpModeEnabled": false, "driverIsOnTrip":false] as [String: Any]
+                                
+                                DataService.instance.createFirebaseDBUser(uid: user.uid, userData: userData, isHelper: true)
+                                
+                            }
+                        }
+                        
+                        self.dismiss(animated: true, completion: nil) // removing LoginVC
+                    }
+                    
+                    // creating a new user
+                    else{
+                        
+                        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                            
+                            if error != nil{
+                                
+                                if let errorCode = AuthErrorCode(rawValue: error!._code){
+                                    switch errorCode {
+                                    case :
+                                        <#code#>
+                                    default:
+                                        <#code#>
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
